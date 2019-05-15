05940f428ff6ada66641d1c9208ade68352f2eb5
zhangys1989 <zhangys#@1234>
2019-05-15 16:46:08 +0800

    test1

---
+++ src/main/java/pages/test1.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
+
+/**
+ * Created by zhangys。
+ */
+
+public class test1 {
+
+
+}
